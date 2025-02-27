768e082f4a33c2355da4a2a9c9dc60d71c2d2dab
Juul <juul20018@gmail.com>
2025-02-27 22:39:43 +0100

    concatenating 2 dicts and writing the full data dict to yaml file

--- parse_yaml.py
+++ parse_yaml.py
@@ -4,3 +4,18 @@ with open('hp_test.yaml', 'r') as f:
     data = yaml.load(f, Loader=yaml.SafeLoader)
 
 print(data)
+
+# writing to a YAML file
+data1 = {
+    'name': 'test',
+    'age': 20,
+    'city': 'Ghent',
+    'skills': ['Python', 'Data Analysis', 'Machine Learning']
+}
+
+data.update(data1)
+
+with open('hp_test.yaml', 'w') as f:
+    yaml.dump(data, f)
+
+print("Data has been written to yaml file")
