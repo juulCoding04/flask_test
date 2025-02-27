d782b05a1d455d4df326b1651b272ba20746ecde
Juul <juul20018@gmail.com>
2025-02-27 22:17:08 +0100

    learning about templates in flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -9,5 +9,10 @@ def hello_world():
     return "<p>Hello, World!</p>"
 # hello function is visible from 127.0.0.1/5000/<name>
 @app.route("/<name>")
-def hello(name):
+def name(name):
     return f"Hello, {escape(name)}!"
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template('hello.html', person=name)
